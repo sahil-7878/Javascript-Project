39f53bfb2d30c55518fa9e71f3dfa3ec87ad0f05
Mr Sahil <mrsahil8316@gmail.com>
2025-08-05 12:55:29 +0530

    Add files via upload

---
+++ Javascript-main/Calculator.js
@@ -0,0 +1,31 @@
+let number1 = 25
+let number2 = 50
+
+const addition = number1 + number2
+
+const substraction = number1 - number2
+
+const Division = number1 / number2
+
+const Multiplication = number1 * number2
+
+const Modulus = number1 % number2
+
+console.log("----------simple calculation----------------")
+
+
+
+
+console.log(`Addition of two numbers is: ${addition}`)
+
+console.log("------------------")
+console.log(`Substraction of two numbers is: ${substraction}`)
+
+console.log("------------------")
+console.log(`Division of two numbers is: ${Division}`)
+
+console.log("------------------")
+console.log(`Multiplication of two numbers is: ${Multiplication}`)
+
+console.log("------------------")
+console.log(`Modulus of two numbers is: ${Modulus}`)

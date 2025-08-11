a49912525eb457cceb24cfbb3d83027e21ad2483
Mr Sahil <mrsahil8316@gmail.com>
2025-08-11 11:07:55 +0530

    Add files via upload

---
+++ Project-1/Calculator.js
@@ -0,0 +1,20 @@
+const num1 = 25;
+const num2 = 15;
+
+let ans;
+
+console.log(" ==== Simple Calculator ==== ")
+ans = num1 + num2;
+console.log(`Additon: ${num1} + ${num2} = ${ans}`);
+
+ans = num1 - num2;
+console.log(`Subtraction: ${num1} - ${num2} = ${ans}`);
+
+ans = num1 * num2;
+console.log(`Multiplication: ${num1} * ${num2} = ${ans}`);
+
+ans = num1 / num2;
+console.log(`Division: ${num1} / ${num2} = ${ans}`);
+
+ans = num1 % num2;
+console.log(`Modulus: ${num1} % ${num2} = ${ans}`);
